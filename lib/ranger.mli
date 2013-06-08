0525b1a229757a1c57849f8fb3fd32d5b8aa9269
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-08 14:01:12 -0400

    mli

---
+++ lib/ranger.mli
@@ -0,0 +1,23 @@
+type 'a t
+
+val create : ?start:int -> stop:int -> (int -> 'a) -> 'a t
+
+val of_array : ?start:int -> ?stop:int -> 'a array -> 'a t
+
+val of_string : ?start:int -> ?stop:int -> string -> char t
+
+val iter : 'a t -> f:('a -> unit) -> unit
+
+val iteri : 'a t -> f:(int -> 'a -> unit) -> unit
+
+val bounds : 'a t -> int * int
+
+val for_all : 'a t -> f:('a -> bool) -> bool
+
+val length : 'a t -> int
+
+val reverse : 'a t -> 'a t
+
+val fold_left : 'a t -> init:'b -> f:('b -> 'a -> 'b) -> 'b
+
+val fold_right : 'a t -> f:('a -> 'b -> 'b) -> init:'b -> 'b
