0362e60b4b1b2380b9d440ff1030732ec81d2705
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-06-10 20:15:39 -0400

    for_all tests

--- lib_test/ranger_test.ml
+++ lib_test/ranger_test.ml
@@ -4,6 +4,10 @@ module R = Ranger
 
 let arr = [|1;2;3;4|]
 
+let arr_gen = 
+  let open Gen in
+  array (make_int 0 10) (make_int 4 10)
+
 let () = Test.add_simple_test ~title:"of_array"
     (fun () ->
        let rng = R.of_array arr ~start:1 ~stop:2 in
@@ -16,6 +20,11 @@ let () = Test.add_simple_test ~title:"reverse"
        let elems = R.to_list rev in
        Assert.equal arr (Array.of_list (List.rev elems)))
 
+let () = Test.add_random_test ~title:"for_all" ~nb_runs:20 arr_gen
+    (fun arr -> R.of_array arr)
+    [
+      Spec.always ==> (fun r -> R.for_all r ~f:(fun x -> x > 3))
+    ]
 
 let () = Test.launch_tests ()
 
